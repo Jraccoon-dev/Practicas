ad7bc19ae5298d1b833f533cf0178a5e6a654a2a
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-04-11 18:49:59 -0500

    changes

---
+++ VueLearn/firstapp/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+<div v-if="showName"><p>{{user.first_name}}</p></div>
+<div v-else><p>No set Name</p></div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      title: 'Hola VueJs',
+      user: {
+        first_name: 'Jose',
+        last_name: 'McMilan'
+      },
+      myHtml: '<h1>Hola Barco </h1>',
+      showName: false
+    }
+  }
+}
+</script>
+
+<style media="screen">
+</style>
